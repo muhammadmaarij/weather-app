40b576fc5c94a375ab36fd68188d9c925c1c804b
Muhammad Maarij <muhammadmaarij72gmail.com>
2023-07-18 15:15:56 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Card from "./components/Card";
 
-function App() {
+const API_KEY = "YOUR_OPENWEATHERMAP_API_KEY";
+
+const WeatherApp = () => {
+  const [selectedCardIndex, setSelectedCardIndex] = useState(null);
+
+  const handleCardClick = (index) => {
+    setSelectedCardIndex(index);
+  };
+
+  const [city, setCity] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
+
+  const handleCityChange = (event) => {
+    setCity(event.target.value);
+  };
+
+  const fetchWeatherData = () => {
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
+    )
+      .then((response) => response.json)
+      .then((response) => {
+        setWeatherData(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching weather data:", error);
+        setWeatherData(null);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <h1>Weather App</h1>
+      <div>
+        <input
+          type="text"
+          value={city}
+          onChange={handleCityChange}
+          placeholder="Enter city name"
+        />
+        <button onClick={fetchWeatherData}>Get Weather</button>
+      </div>
+      {weatherData && (
+        <div>
+          <h2>Weather for {weatherData.name}</h2>
+          <p>Temperature: {weatherData.main.temp}°C</p>
+          <p>Description: {weatherData.weather[0].description}</p>
+          <p>Humidity: {weatherData.main.humidity}%</p>
+          <p>Wind Speed: {weatherData.wind.speed} m/s</p>
+        </div>
+      )} */}
+      <Card
+        day={"Monday"}
+        weatherIcon={""}
+        high={22}
+        low={25}
+        isSelected={selectedCardIndex === 0}
+        onClick={() => handleCardClick(0)}
+      />
+      <Card
+        day={"Tuesday"}
+        weatherIcon={""}
+        high={20}
+        low={23}
+        isSelected={selectedCardIndex === 1}
+        onClick={() => handleCardClick(1)}
+      />
+      <Card
+        day={"Tuesday"}
+        weatherIcon={""}
+        high={20}
+        low={23}
+        isSelected={selectedCardIndex === 2}
+        onClick={() => handleCardClick(2)}
+      />
+      <Card
+        day={"Tuesday"}
+        weatherIcon={""}
+        high={20}
+        low={23}
+        isSelected={selectedCardIndex === 3}
+        onClick={() => handleCardClick(3)}
+      />
     </div>
   );
-}
+};
 
-export default App;
+export default WeatherApp;
---
+++ src/components/Card.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "../Card.css";
+
+function Card({ day, weatherIcon, high, low, isSelected, onClick }) {
+  const weatherIconStyle = {
+    height: "180px",
+    width: "140px",
+    borderRadius: "10px",
+    border: isSelected ? "2px solid #ccc" : "2px solid transparent",
+    /* Add any other styles you want */
+  };
+
+  return (
+    <div style={weatherIconStyle} onClick={onClick}>
+      <div className="parent-div">
+        <p className="day">{day}</p>
+        <img className="image-temp" src={weatherIcon} alt="Weather Icon" />
+        <div className="row-temp">
+          <p className="text-temp-low">{low}°</p>
+          <p className="text-temp">{high}°</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
