2d051c85df807d71c83ac6a2d50d729db0be13b8
Muhammad Maarij <muhammadmaarij72gmail.com>
2023-07-22 14:44:33 +0500

    first commit

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -3,67 +3,73 @@ import { Bar } from "react-chartjs-2";
 
 function BarChart({ data2 }) {
   console.log("data2", data2);
-  const chartData = {
-    labels: Object.keys(data2),
-    datasets: [
-      {
-        label: "Pollution Data",
-        data: Object.values(data2),
-        backgroundColor: "rgba(75,192,192,1)",
-      },
-    ],
-  };
+  let chartData;
+  if (data2 == null) {
+  } else {
+    chartData = {
+      labels: Object.keys(data2),
+      datasets: [
+        {
+          label: "Pollution Data",
+          data: Object.values(data2),
+          backgroundColor: "darkslateblue",
+        },
+      ],
+    };
+  }
   return (
     <div>
-      <div style={{ margin: "200px" }}>
-        <Bar
-          data={chartData}
-          options={{
-            plugins: {
-              title: {
-                display: true,
-                text: "Air Pollution Stats",
-                font: {
-                  size: 32,
-                },
-                legend: {
+      {data2 && (
+        <div style={{ margin: "200px" }}>
+          <Bar
+            data={chartData}
+            options={{
+              plugins: {
+                title: {
                   display: true,
-                  position: "top", // Position of the legend: 'top', 'left', 'bottom', or 'right'
-                  labels: {
-                    font: {
-                      size: 12,
+                  text: "Air Pollution Stats",
+                  font: {
+                    size: 32,
+                  },
+                  legend: {
+                    display: true,
+                    position: "top", // Position of the legend: 'top', 'left', 'bottom', or 'right'
+                    labels: {
+                      font: {
+                        size: 12,
+                      },
                     },
                   },
                 },
               },
-            },
-            scales: {
-              x: {
-                grid: {
-                  display: false, // Display the vertical grid lines
-                },
-                ticks: {
-                  font: {
-                    size: 20,
+              scales: {
+                x: {
+                  grid: {
+                    display: false, // Display the vertical grid lines
+                  },
+                  ticks: {
+                    font: {
+                      size: 20,
+                    },
                   },
                 },
-              },
 
-              y: {
-                grid: {
-                  display: false, // Display the horizontal grid lines
-                },
-                beginAtZero: true, // Start the y-axis at zero
-                ticks: {
-                  font: {
-                    size: 20,
+                y: {
+                  grid: {
+                    display: false, // Display the horizontal grid lines
+                  },
+                  beginAtZero: true, // Start the y-axis at zero
+                  ticks: {
+                    font: {
+                      size: 20,
+                    },
                   },
                 },
               },
-            },
-          }}
-        />
-      </div>
+            }}
+          />
+        </div>
+      )}
     </div>
   );
 }
